7712cdf81b91f231f94096dcf7a702c9d96b3bfa
Hangkun Ung <hangkun_ung@brown.edu>
2016-03-20 13:58:06 -0400

    bubble sort implemented

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -8,7 +8,24 @@ public class BubbleSort {
 	 */
 	public static int[] bubbleSort(int[] unsortedArray) {
 		//TODO: S1 implements bubble sort
-		
-		return null;
+		int size = unsortedArray.length;
+		boolean swapped = true;
+		while (swapped) {
+			swapped = false;
+			for (int i =1; i< size; i++) {
+				if (unsortedArray[i-1] > unsortedArray[i]) {
+					swap(unsortedArray, i-1, i);
+					swapped = true;
+				}
+			}
+			size = size -1;
+		}
+		return unsortedArray;
+	}
+
+	private static void swap(int array[], int i, int j) {
+		int tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
 	}
 }
